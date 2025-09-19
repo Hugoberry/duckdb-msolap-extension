44e98ff193037d17e16bc6f27ca204dfb21a7105
Igor Cotruta <hugoberry314@gmail.com>
2025-09-19 13:54:38 +0100

    initial build

--- src/msolap_extension.cpp
+++ src/msolap_extension.cpp
@@ -3,40 +3,27 @@
 #include "msolap_extension.hpp"
 #include "msolap_scanner.hpp"
 #include "msolap_utils.hpp"
-#include "duckdb/main/extension_util.hpp"
 #include "duckdb/parser/parsed_data/create_table_function_info.hpp"
 
 namespace duckdb {
 
-static void LoadInternal(DatabaseInstance &instance) {
+static void LoadInternal(ExtensionLoader &loader) {
     // Register MSOLAP table function
     MSOLAPScanFunction msolap_scan_fun;
-    ExtensionUtil::RegisterFunction(instance, msolap_scan_fun);
+    loader.RegisterFunction(msolap_scan_fun);
 }
 
-void MsolapExtension::Load(DuckDB &db) {
-    LoadInternal(*db.instance);
+void MsolapExtension::Load(ExtensionLoader &loader) {
+    LoadInternal(loader);
 }
 
-std::string MsolapExtension::Name() {
-    return "msolap";
-}
-
-std::string MsolapExtension::Version() const {
-#ifdef EXT_VERSION_MSOLAP
-    return EXT_VERSION_MSOLAP;
-#else
-    return "";
-#endif
-}
 
 } // namespace duckdb
 
 extern "C" {
 
-DUCKDB_EXTENSION_API void msolap_init(duckdb::DatabaseInstance &db) {
-    duckdb::DuckDB db_wrapper(db);
-    db_wrapper.LoadExtension<duckdb::MsolapExtension>();
+DUCKDB_CPP_EXTENSION_ENTRY(msolap, loader) {
+    duckdb::MsolapExtension::Load(loader);
 }
 
 DUCKDB_EXTENSION_API const char *msolap_version() {
